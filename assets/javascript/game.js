1f73407bde29bac24f7787a9472bf5e12eb1f9e3
taqwa <trushdan@gmail.com>
2017-12-15 01:08:46 -0500

    changed some styles, replaces imgs, and some JS edits.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,7 @@ var winCounter = 0;
 var lossCounter = 0;
 
 
-/////FUNCTIONS
+/////FUNCTION
 //==================================
 
 /////goal number
@@ -20,22 +20,17 @@ function goalNumGen() {
   console.log(numberGoal);
 };
 
+//===================================
+
 //for loop that assigns each img a random number value.
   for (var i = 0; i < 4; i++) {
     var crystalNumber = Math.floor(Math.random() * (12 - 1 + 1)) +1;
     console.log('crystalNumber: ', crystalNumber);
     $(".crystal-img").attr('data-random-num', crystalNumber);
-
     ///add another for loop here
-    };
-
+    }
 
   $("#total-points").html("<p>Total: " + totalPoints + "</p>");
-
-// function totalPointsAdder() {
-  // totalPoints += crystalNumber; //totalPoints = totalPoints+crystalNumber
-//   console.log(totalPoints);
-// }
 
 /////on click
 $(".crystal-img").click(function(event) {
@@ -43,51 +38,27 @@ $(".crystal-img").click(function(event) {
       $("#total-points").html("<p>Total: " + totalPoints + "</p>");
       console.log($(this).attr('data-random-num'));
 
-      ////if statements --> add this to the onClick event
+      ////if statements
       if (totalPoints === numberGoal) {
-          $("#wins").append("<p>"+ ++winCounter +"</p>");
+          var win = 1;
+          $("#wins").after(winCounter += win);
+          reset();
         } else if (totalPoints > numberGoal) {
-          $("#losses").append("<p>"+ ++lossCounter +"</p>");
+          var lose = 1;
+          $("#losses").after(lossCounter += lose);
+          reset();
         };
   });
 
 
-////if statements --> add this to the onClick event
-// if (totalPoints === numberGoal) {
-//     $("#wins").append(++winCounter);
-//   } else if (totalPoints > numberGoal) {
-//     $("#losses").append(++loseCounter);
-//   };
-
 console.log(totalPoints);
 
 
-// function reset() {};
+function reset() {
+  goalNumGen();
+  totalPoints = 0;
+};
 
-
-/////MAIN PROCESS
 //==================================
 
 goalNumGen();
-
-
-
-
-//////////////////////////////////
-
-//OLD FOR LOOP:
-// var crystalNumber = Math.floor(Math.random() * (12 - 1 +1)) + 1;
-// console.log('crystalNumber: ', crystalNumber);
-//
-// var crystal = $("<div>");
-//     crystal.attr({
-//       "class": 'crystal',
-//       "random-num": crystalNumber
-//     });
-// $('.crystals').prepend(crystal);
-
-//OLD TOTALPOINTSADDER FUNCTION:
-/// function totalPointsAdder() {
-  // totalPoints += crystalNumber; //totalPoints = totalPoints+crystalNumber
-//   console.log(totalPoints);
-// }
